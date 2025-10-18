dbabec64f6701d24ad9148916215ee552a7659b3
Josh Stein <josh.e.stein@gmail.com>
2025-10-18 14:58:52 +0200

    Start working on distance calc

--- main.py
+++ main.py
@@ -10,10 +10,56 @@ PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
+FINGER_INDEXES = [
+    15,  # left wrist
+    16,  # right wrist
+    17,  # left pinky
+    18,  # right pinky
+    19,  # left index
+    20,  # right index
+    21,  # left thumb
+    22,  # right thumb
+]
+
+FACE_INDEXES = [
+    0,  # nose
+    2,  # left eye
+    5,  # right eye
+    7,  # left ear
+    8,  # right ear
+    9,  # mouth (left)
+    10,  # mouth (right)
+]
+
+DISTANCE_THRESHOLD = 0.65
+
+
+def calculate_distance(first, second):
+    return abs(first.x - second.x) + abs(first.y - second.y) + abs(first.z - second.z)
+
+
 def pose_detection_callback(
     result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int
 ):
-    print(result)
+    if not result.pose_landmarks:
+        return
+
+    normalized_landmarks = result.pose_landmarks[0]
+
+    for finger_index in FINGER_INDEXES:
+        if normalized_landmarks[finger_index].presence < 0.9:
+            continue
+
+        for face_index in FACE_INDEXES:
+            if normalized_landmarks[face_index].presence < 0.9:
+                continue
+
+            distance = calculate_distance(
+                normalized_landmarks[finger_index], normalized_landmarks[face_index]
+            )
+
+            if distance < DISTANCE_THRESHOLD:
+                print(distance)
 
 
 def initialise_detector():
