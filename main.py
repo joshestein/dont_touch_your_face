4cea7cf306e323377da66c690051c772f617b9ae
Josh Stein <josh.e.stein@gmail.com>
2025-10-18 14:08:32 +0200

    Remove FaceDetector
    If we only use PoseDetector I think we can still get enough information
    with half the computational complexity. Plus, we don't have to worry
    about syncing results between two models.

--- main.py
+++ main.py
@@ -4,47 +4,30 @@ import mediapipe as mp
 
 BaseOptions = mp.tasks.BaseOptions
 FaceDetector = mp.tasks.vision.FaceDetector
-FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
-FaceDetectorResult = mp.tasks.vision.FaceDetectorResult
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
 PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
 PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-def face_detection_callback(
-    result: FaceDetectorResult, output_image: mp.Image, timestamp_ms: int
-):
-    print(result)
-
-
 def pose_detection_callback(
-    result: FaceDetectorResult, output_image: mp.Image, timestamp_ms: int
+    result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int
 ):
     print(result)
 
 
-def initialise_detectors():
-    face_options = FaceDetectorOptions(
-        base_options=BaseOptions(
-            model_asset_path="models/blaze_face_short_range.tflite"
-        ),
-        running_mode=VisionRunningMode.LIVE_STREAM,
-        result_callback=face_detection_callback,
-    )
-    face_detector = FaceDetector.create_from_options(face_options)
-
+def initialise_detector():
     pose_options = PoseLandmarkerOptions(
         base_options=BaseOptions(model_asset_path="models/pose_landmarker_lite.task"),
         running_mode=VisionRunningMode.LIVE_STREAM,
         result_callback=pose_detection_callback,
     )
     pose_detector = PoseLandmarker.create_from_options(pose_options)
-    return face_detector, pose_detector
+    return pose_detector
 
 
 def main():
-    face_detector, pose_detector = initialise_detectors()
+    pose_detector = initialise_detector()
     count = 0
     cap = cv.VideoCapture(0)
 
@@ -66,10 +49,8 @@ def main():
         if cv.waitKey(1) == ord("q"):
             break
 
-        face_detector.detect_async(image, count)
         pose_detector.detect_async(image, count)
 
-    face_detector.close()
     pose_detector.close()
     cap.release()
     cv.destroyAllWindows()
